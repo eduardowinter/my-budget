ceb01c6886e650f497a2f20636e40bb669e5c883
Eduardo Winter <eduardowb@gmail.com>
2022-10-12 11:32:44 -0300

    Added header, summary, and content table

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,35 @@
+import { SummaryCard, SummaryContainer } from "./syles";
+import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from 'phosphor-react'
+
+export function Summary() {
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Incomes</span>
+          <ArrowCircleUp size={32} color="#00b37e" />
+        </header>
+
+        <strong>$ 12.700,00</strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Outcomes</span>
+          <ArrowCircleDown size={32} color="#f75a68" />
+        </header>
+
+        <strong>$ 12.700,00</strong>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#fff" />
+        </header>
+
+        <strong>$ 12.700,00</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  )
+}
---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,40 @@
+import { Header } from "../../components/Header"
+import { Summary } from "../../components/Summary"
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles"
+
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Website development</td>
+              <td>
+                <PriceHighlight variant="income">
+                  $ 4.000,00
+                </PriceHighlight>
+              </td>
+              
+              <td>Sales</td>
+              <td>10/10/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Tacos</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - $ 39,00
+                </PriceHighlight>
+              </td>
+              <td>Food</td>
+              <td>10/10/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  )
+}
